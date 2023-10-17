6bb57ff536d78564a6bd45239597acde3c35a9a1
Lena-Charriere <lena.chrr@yahoo.com>
2023-10-17 10:14:12 +0200

    _ avancement personnage dans window

--- src/fr/miroff/FouleProject/character/Personnage.java
+++ src/fr/miroff/FouleProject/character/Personnage.java
@@ -10,19 +10,33 @@ public class Personnage {
         this.y = y;
     }
 
-    public void move() {
-        // TODO: 16/10/2023 Do something that work to move characters INSIDE THE SCREEN and HANDLE COLLISIONS
+    public int getX() {
+        return x;
+    }
 
-        if (Math.random() < 0.5 || x < -1000) {
-            x += 1;
-        } else {
+    public int getY() {
+        return y;
+    }
+
+    public void move() {
+        // Mouvement sur l'axe X
+        if (Math.random() < 0.5) {
+            if (x < 1000) {
+                x += 1;
+            }
+        } else if (x > 0) {
             x -= 1;
         }
 
-        if (Math.random() < 0.5 || y < 1000) {
-            y += 1;
-        } else
+        // Mouvement sur l'axe Y
+        if (Math.random() < 0.5) {
+            // supposons que 1000 est la hauteur maximale de l'écran
+            if (y < 1000) {
+                y += 1;
+            }
+        } else if (y > 0) {
             y -= 1;
+        }
     }
 
     private boolean isColliding(){
