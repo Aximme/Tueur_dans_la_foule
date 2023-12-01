6b5a49666df320359481ae25a1abb621b8db9151
simon <41833744+S1M0Z@users.noreply.github.com>
2023-12-01 16:45:52 +0100

    fix double window

--- src/fr/miroff/FouleProject/Main.java
+++ src/fr/miroff/FouleProject/Main.java
@@ -19,7 +19,7 @@ public class Main {
         buildingsReadyLatch.countDown();
     });
 
-        SwingUtilities.invokeLater(() -> mainWindow = new Window());
+        //SwingUtilities.invokeLater(() -> mainWindow = new Window());
 
 
 
